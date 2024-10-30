a2a886fb3ee205244e48b2eefba883cba0efda95
djigue <yanmaubrun@gmail.com>
2024-10-30 15:01:28 +0100

    commit avant le node

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 let toDoList = [];
 const options = ["Toutes", "Actives", "Terminées"];
+const bouton = document.createElement("button");
+bouton.style.cursor = "pointer";
 
 document.addEventListener("DOMContentLoaded", function() {
     fetchEtListerTaches();
@@ -14,7 +16,11 @@ async function myJson(url) {
     console.log("Données fetchées:", data);
 
     if (data.taches) {
-        toDoList = data.taches; 
+        toDoList = data.taches.sort((a, b) => {
+            const dateA = new Date(a.date.split("/").reverse().join("-"));
+            const dateB = new Date(b.date.split("/").reverse().join("-"));
+            return dateA - dateB;
+        });
     }
     console.log("Taches chargés:", toDoList);
     return data;
@@ -42,7 +48,6 @@ function lancement() {
         afficherMenu();
     }
 
-    //lancement();
 
 function afficherMenu() {
 
@@ -67,6 +72,14 @@ function afficherMenu() {
 
     menuDiv.appendChild(ul);
 
+    bouton.textContent = "Créer une nouvelle tâche";
+    
+
+    menuDiv.appendChild(bouton);
+
+    bouton.addEventListener('click', function(e) {
+       ajouterTache();
+    })
 }
 
 function creerCheckbox() {
@@ -91,3 +104,119 @@ function creerCheckbox() {
         container.appendChild(label);
     });
 }
+
+function ajouterTache () {
+
+    const menuDiv = document.getElementById("container");
+    menuDiv.innerHTML = "";
+
+    const titre = document.createElement("h1");
+    titre.textContent = "Quelle tâche voulez vous ajouter";
+    titre.style.color = "#B22430";
+    menuDiv.style.textAlign = "center";
+    menuDiv.appendChild(titre);
+
+    const form = document.createElement("form");
+
+    const labelNom = document.createElement("label");
+    labelNom.textContent = "Quel nom voulez vous donner à votre nouvelle tâche :"
+    form.appendChild(labelNom);
+
+    const nom = document.createElement("input");
+    nom.placeholder = "Nom de la tâche";
+    form.appendChild(nom);
+    nom.style.marginTop = "5%";
+    nom.setAttribute('required', '');
+    form.appendChild(nom);
+
+    const brInput = document.createElement("br");
+    form.appendChild(brInput);
+
+    const labelDescription = document.createElement("label");
+    labelDescription.textContent = "Décrivez votre tâche :";
+    form.appendChild(labelDescription);
+
+    const description = document.createElement("textarea");
+    description.placeholder = "Description de la tâche";
+    form.appendChild(description);
+    description.style.marginTop = "5%";
+    description.setAttribute('required', '');
+    form.appendChild(description);
+
+    const brInput1 = document.createElement("br");
+    form.appendChild(brInput1);
+
+    const labelDate = document.createElement("label");
+    labelDate.textContent = "Quelle est la date limite :"
+    form.appendChild(labelDate);
+
+    const date = document.createElement("input");
+    date.setAttribute('type','tel');
+    date.placeholder = "jj/mm/aaaa";
+    date.setAttribute('required', '');
+    form.appendChild(date);
+
+    form.appendChild(date);
+    date.style.marginTop = "5%";
+    
+    const brInput2 = document.createElement("br");
+    form.appendChild(brInput2);
+
+    menuDiv.appendChild(form);
+
+    bouton.textContent = "Ajouter la tâche";
+
+    menuDiv.appendChild(bouton);
+
+    bouton.addEventListener("click", function(e) {
+        e.preventDefault();
+        const nom = nom.value;
+        const description = description.value;
+        const date = date.value;
+
+        if (nom && description && date && form.checkValidity()) {
+            const newTache = {
+                nom: nom,
+                description: description,
+                date: date,
+            };
+            
+            // Envoie des données au serveur
+            fetch('http://localhost:3000/ajouter-tache', {
+
+                //method atendue par le serveur (voir server.js l14)
+                method: 'POST',
+                //information importante pour que le server notament le type de fichier
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                //contenu de la requete 
+                body: JSON.stringify(newTache),
+            })
+
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erreur lors de l\'ajout de la tâche');
+                }
+                return response.text();//transforme la reponse en chaine de charactère
+    
+            })
+
+            //message = responsetext()
+            .then(message => {
+                console.log("message :" + message);
+                contacts.push(newTache); 
+                fetchEtListerTaches(toDoList); 
+                alert("Nouvelle tâche ajoutée avec succès");
+
+            })
+            .catch(error => {
+                console.error(error);
+                alert("Erreur lors de l'ajout de la tâche");
+            });
+        } else {
+            alert("Respectez le format imposé!! (on a pas galerer pour rien)");
+        }
+    }); 
+}
+
