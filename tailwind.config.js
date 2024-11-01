0dc6a1bd53a0fa8f67cf633afda2de7e9d83216d
djigue <yanmaubrun@gmail.com>
2024-11-01 15:54:37 +0100

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", 
+    "./**/*.js",
+    "!./node_modules/**/*" 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
